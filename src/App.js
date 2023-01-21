300aef4b5ad78b3e65647dc9fc538b34006b315b
Kelsey <kelseymedved@gmail.com>
2023-01-21 16:03:09 -0500

    addes images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import flower from "./icons/flower.png";
+import pink from "./icons/flower-pink.png";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Folks</h1>
+      <a
+        type="hidden"
+        href="https://www.flaticon.com/free-icons/flower"
+        title="flower icons"
+      >
+        <img className="flower-power" alt="flower-icon" src={flower} />
+        <img className="pink-flower" src={pink} alt="pink-flower" />
+        Flower icons created by Freepik - Flaticon
+      </a>
     </div>
   );
 }
