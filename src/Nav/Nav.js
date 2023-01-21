8e34627a4727bd22f0ec3510d2ff4b98eb853896
Kelsey <kelseymedved@gmail.com>
2023-01-21 16:27:44 -0500

    simple nav

---
+++ src/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+import flower from "../icons/flower.png";
+import pink from "../icons/flower-pink.png";
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <img className="flower-power" alt="flower-icon" src={flower} />
+      <h1 className="header-name">Kelsey Dickson</h1>
+      <img className="pink-flower" src={pink} alt="pink-flower" />
+    </div>
+  );
+};
+
+export default Nav;
