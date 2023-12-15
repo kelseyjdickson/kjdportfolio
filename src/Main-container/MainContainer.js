ec2e5bb8165963246ce897b5aaaa4471d7eb22ab
Kelsey Dickson <kelseymedved@gmail.com>
2023-12-15 16:17:22 -0500

    Updated Styles

--- src/Main-container/MainContainer.js
+++ src/Main-container/MainContainer.js
@@ -2,10 +2,20 @@ import React from "react";
 
 const MainContainer = () => {
   return (
-    <div className="div-container">
-      <div className="child box1">Box1</div>
-      <div className="child box2">Box2</div>
-    </div>
+    <>
+      <div className="main-container">
+        <div className="child box3">
+          Combining technical experience within the healthcare sector for over 7
+          years, I have a genuine passion for teamwork and collaboration. I
+          appreciate and recognize the power of cross-functional cooperation,
+          and leveraging diverse skills to deliver successful outcomes.
+        </div>
+        <div className="div-container">
+          <div className="child box1">PROJECT: Women In Tech</div>
+          <div className="child box2">Project: Pizza Store</div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,6 +6,8 @@ const Nav = () => {
     <div className="nav-container">
       <img className="flower-power" alt="flower-icon" src={flower} />
       <h1 className="header-name">Kelsey Dickson</h1>
+      <h3 className="header-name">About</h3>
+      <h3 className="header-name">Projects</h3>
       <img className="pink-flower" src={flower} alt="pink-flower" />
     </div>
   );
